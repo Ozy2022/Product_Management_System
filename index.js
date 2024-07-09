f131e016806428be808adb1c5dea9b5858b5995c
Osama <ozy.almondry2021@gmail.com>
2024-07-09 23:23:18 +0300

    Created showData function to read the data in the UI

--- index.js
+++ index.js
@@ -72,6 +72,7 @@ submit.onclick = function () {
   localStorage.setItem("product", JSON.stringify(dataProduct));
 
   clearData();
+  showData();
 };
 
 //# Clear inputs
@@ -87,3 +88,31 @@ function clearData() {
   count.value = "";
   category.value = "";
 }
+
+//# Read data
+
+function showData() {
+  // note that if you have an arrey data, and if want to show them in the UI you must rernder it by a loop
+
+  let table = "";
+  for (let i = 0; i < dataProduct.length; i++) {
+    table += `
+        <tr>
+            <td>${i}</td>
+            <td>${dataProduct[i].title}</td>
+            <td>${dataProduct[i].price}</td>
+            <td>${dataProduct[i].taxes}</td>
+            <td>${dataProduct[i].ads}</td>
+            <td>${dataProduct[i].discount}</td>
+            <td>${dataProduct[i].total}</td>
+            <td>${dataProduct[i].category}</td>
+            <td><button id="update">update</button></td>
+            <td><button id="delete">delete</button></td>
+        </tr>
+        `;
+  }
+
+  document.getElementById("tbody").innerHTML = table;
+}
+
+showData();
